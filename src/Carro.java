3d2a1c108bbc478d6b23a715de462826186daea1
MenezesMarlon <contato.menezesmarlon@gmail.com>
2023-01-02 19:54:00 -0300

    Criação do Objeto Carro

---
+++ src/Carro.java
@@ -0,0 +1,11 @@
+public class Carro {
+    String marca;
+    String modelo;
+    String cor;
+    float motor;
+    int aro;
+    int combustivel = 0;
+    boolean turbo = false;
+    boolean partida = false;
+
+}
