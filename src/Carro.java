793c8606e2a444682ec4072ce0402372bd56c11b
MenezesMarlon <contato.menezesmarlon@gmail.com>
2023-01-02 20:24:01 -0300

    Adição de comentários para futuros métodos para Tunagem do carro

--- src/Carro.java
+++ src/Carro.java
@@ -1,18 +1,49 @@
+import java.util.Scanner;
+
 public class Carro {
+    Scanner scanner = new Scanner(System.in);
     String marca;
     String modelo;
     String cor;
     float motor;
     int aro;
     int combustivel = 0;
-    boolean turbo = false;
     boolean partida = false;
+//    boolean turbo = false;
+//    boolean rebaixado = false;
 
     void status(){
         System.out.println(this.marca + " " + this.modelo);
         System.out.println(this.cor);
         System.out.println("Aro "+ this.aro);
         System.out.println("Motor " + this.motor);
+    }
+
+    void ligar(){
+        if (this.combustivel > 5) {
+            System.out.println("Ligando Carro...");
+            System.out.println(this.modelo + " Ligado!");
+        } else {
+            System.out.println("Carro sem combustível!");
+            this.abastecer();
+        }
+    }
+
+    void abastecer(){
+        int valorAbastecido;
+        System.out.println("Quanto de combustivel deseja colocar?");
+        valorAbastecido = scanner.nextInt();
+        this.combustivel = valorAbastecido / 5;
+        System.out.println("Quantidade de litros no tanque: " + this.combustivel);
+        this.ligar();
+
+    }
+
+    void trocaAro() {
+        System.out.print("Qual aro deseja colocar: ");
+        this.aro = scanner.nextInt();
+
+        this.status();
     }
 
 }
