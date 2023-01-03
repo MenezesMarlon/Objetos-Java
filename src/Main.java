6c9985ebb67427d53a8c915797f9b17bc11bd7dc
MenezesMarlon <contato.menezesmarlon@gmail.com>
2023-01-02 21:13:27 -0300

    Updates da Classe Caneta

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
+    Caneta caneta = new Caneta();
+
+    caneta.modelo = "Bic Cristal";
+    caneta.cor = "Preta";
+    caneta.carga = 80;
+    caneta.status();
+    caneta.tampar();
 
     }
+
 }
